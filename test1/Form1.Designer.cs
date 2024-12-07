3301f9e318630c903092fc8e96585ac409da94e9
Tài Nguyễn Tấn <nguyentantai220804@gmail.com>
2024-12-08 00:34:09 +0700

    Add project files.

---
+++ test1/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtGiaVe = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.radTreEm = new System.Windows.Forms.RadioButton();
+            this.radSV = new System.Windows.Forms.RadioButton();
+            this.radNguoiLon = new System.Windows.Forms.RadioButton();
+            this.txtThanhTien = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(28, 38);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(753, 511);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(684, 111);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 363);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(209, 114);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(478, 363);
+            this.panel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel4.Controls.Add(this.groupBox3);
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(513, 555);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(242, 136);
+            this.flowLayoutPanel4.TabIndex = 17;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnThanhToan);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtThanhTien);
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(238, 132);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel3.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(270, 555);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(237, 136);
+            this.flowLayoutPanel3.TabIndex = 16;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 126);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thanh Vien";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Khach Hang Thanh Vien";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(27, 555);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(237, 136);
+            this.flowLayoutPanel2.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtGiaVe);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.radTreEm);
+            this.groupBox1.Controls.Add(this.radSV);
+            this.groupBox1.Controls.Add(this.radNguoiLon);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 126);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loai Ve";
+            // 
+            // txtGiaVe
+            // 
+            this.txtGiaVe.Location = new System.Drawing.Point(51, 97);
+            this.txtGiaVe.Name = "txtGiaVe";
+            this.txtGiaVe.Size = new System.Drawing.Size(137, 20);
+            this.txtGiaVe.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(3, 100);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Gia Ve:";
+            // 
+            // radTreEm
+            // 
+            this.radTreEm.AutoSize = true;
+            this.radTreEm.Location = new System.Drawing.Point(6, 65);
+            this.radTreEm.Name = "radTreEm";
+            this.radTreEm.Size = new System.Drawing.Size(88, 17);
+            this.radTreEm.TabIndex = 13;
+            this.radTreEm.TabStop = true;
+            this.radTreEm.Text = "Tre Em (U12)";
+            this.radTreEm.UseVisualStyleBackColor = true;
+            this.radTreEm.CheckedChanged += new System.EventHandler(this.radTreEm_CheckedChanged_1);
+            // 
+            // radSV
+            // 
+            this.radSV.AutoSize = true;
+            this.radSV.Location = new System.Drawing.Point(6, 42);
+            this.radSV.Name = "radSV";
+            this.radSV.Size = new System.Drawing.Size(70, 17);
+            this.radSV.TabIndex = 12;
+            this.radSV.TabStop = true;
+            this.radSV.Text = "Sinh Vien";
+            this.radSV.UseVisualStyleBackColor = true;
+            this.radSV.CheckedChanged += new System.EventHandler(this.radSV_CheckedChanged_1);
+            // 
+            // radNguoiLon
+            // 
+            this.radNguoiLon.AutoSize = true;
+            this.radNguoiLon.Location = new System.Drawing.Point(6, 19);
+            this.radNguoiLon.Name = "radNguoiLon";
+            this.radNguoiLon.Size = new System.Drawing.Size(74, 17);
+            this.radNguoiLon.TabIndex = 11;
+            this.radNguoiLon.TabStop = true;
+            this.radNguoiLon.Text = "Nguoi Lon";
+            this.radNguoiLon.UseVisualStyleBackColor = true;
+            this.radNguoiLon.CheckedChanged += new System.EventHandler(this.radNguoiLon_CheckedChanged_1);
+            // 
+            // txtThanhTien
+            // 
+            this.txtThanhTien.Location = new System.Drawing.Point(21, 42);
+            this.txtThanhTien.Name = "txtThanhTien";
+            this.txtThanhTien.ReadOnly = true;
+            this.txtThanhTien.Size = new System.Drawing.Size(159, 20);
+            this.txtThanhTien.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ThanhTien";
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(21, 80);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(159, 37);
+            this.btnThanhToan.TabIndex = 2;
+            this.btnThanhToan.Text = "Thanh Toan";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            this.btnThanhToan.Click += new System.EventHandler(this.btnThanhToan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 725);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtGiaVe;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RadioButton radTreEm;
+        private System.Windows.Forms.RadioButton radSV;
+        private System.Windows.Forms.RadioButton radNguoiLon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtThanhTien;
+        private System.Windows.Forms.Button btnThanhToan;
+    }
+}
+
---
+++ test1/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        private decimal giaVe = 0;
+        private int soLuongNguoiLon = 0;
+        private int soLuongSV = 0;
+        private int soLuongTreEm = 0;
+        private decimal giaVeNguoiLon = 100000;  // Giá vé người lớn
+        private decimal giaVeSV = 75000;         // Giá vé sinh viên
+        private decimal giaVeTreEm = 50000;     // Giá vé trẻ em
+        private decimal tongTien = 0;
+        public static string GetAsciiString(int[] numbers)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int number in numbers)
+            {
+                sb.Append((char)number);
+            }
+
+            return sb.ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            khoitaosoghe(11, 15);
+            khoitaoday(11, 1);
+        }
+
+        private void khoitaoday(int v1, int v2)
+        {
+            int x, y = 16, kc = 30, d = 65;
+            for (int i = 0; i < v1; i++)
+            {
+                x = 11;
+                for (int j = 0; j < v2; j++)
+                {
+                    Label lblDay = new Label();
+                    lblDay.Location = new System.Drawing.Point(x, y);
+                    lblDay.Size = new System.Drawing.Size(30, 23);
+                    lblDay.Text = ((char)d).ToString();
+                    lblDay.BackColor = Color.Black;
+                    lblDay.TextAlign = ContentAlignment.MiddleCenter;
+                    lblDay.ForeColor = Color.White;
+                    panel1.Controls.Add(lblDay);
+                    x += kc;
+                }
+                y += kc;
+                d++;
+            }
+
+        }
+
+        private void khoitaosoghe(int v1, int v2)
+        {
+            int x, y = 16, kc = 30, d;
+            for (int i = 0; i < v1; i++)
+            {
+                x = 11;
+                d = 1;
+                for (int j = 0; j < v2; j++)
+                {
+                    Button btnGhe = new Button();
+                    btnGhe.Location = new System.Drawing.Point(x, y);
+                    btnGhe.Size = new System.Drawing.Size(30, 23);
+                    btnGhe.Text = d++.ToString();
+                    btnGhe.BackColor = Color.White;
+                    btnGhe.TextAlign = ContentAlignment.MiddleCenter;
+                    panel2.Controls.Add(btnGhe);
+                    btnGhe.Click += BtnGhe_Click;
+                    x += kc;
+                }
+                y += kc;
+            }
+        }
+
+        private void BtnGhe_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (!radNguoiLon.Checked && !radSV.Checked && !radTreEm.Checked)
+            {
+                MessageBox.Show("Vui lòng chọn loại vé trước khi chọn ghế!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;  
+            }
+
+            if (b.BackColor == Color.Gray)
+            {
+                MessageBox.Show("Ghế đã đặt");
+            }
+            else if (b.BackColor == Color.White)
+            {
+                b.BackColor = Color.Red;
+
+                // Tăng số lượng ghế tùy theo loại vé đã chọn
+                if (radNguoiLon.Checked)
+                {
+                    soLuongNguoiLon++;
+                }
+                else if (radSV.Checked)
+                {
+                    soLuongSV++;
+                }
+                else if (radTreEm.Checked)
+                {
+                    soLuongTreEm++;
+                }
+
+                // Tính lại tổng tiền sau khi chọn ghế
+                TinhThanhTien();
+            }
+            else if (b.BackColor == Color.Red)
+            {
+                b.BackColor = Color.White;
+
+                // Giảm số lượng ghế tùy theo loại vé đã chọn
+                if (radNguoiLon.Checked)
+                {
+                    soLuongNguoiLon--;
+                }
+                else if (radSV.Checked)
+                {
+                    soLuongSV--;
+                }
+                else if (radTreEm.Checked)
+                {
+                    soLuongTreEm--;
+                }
+
+                // Tính lại tổng tiền sau khi bỏ chọn ghế
+                TinhThanhTien();
+            }
+        }
+        private void TinhGiaVe()
+        {
+            decimal giaVe = 0;
+
+            // Kiểm tra loại vé đã chọn và gán giá tiền tương ứng
+            if (radNguoiLon.Checked)
+            {
+                giaVe = 100000; 
+            }
+            else if (radSV.Checked)
+            {
+                giaVe = 75000; 
+            }
+            else if (radTreEm.Checked)
+            {
+                giaVe = 50000; 
+            }
+
+            // Cập nhật giá vé vào TextBox "Giá Tiền"
+            txtGiaVe.Text = giaVe.ToString();  // Format thành tiền tệ
+
+            // Tính số lượng ghế đã chọn
+            int soLuongGhe = 0;
+            foreach (Control control in panel2.Controls)
+            {
+                if (control is Button btn && btn.BackColor == Color.Red) // Kiểm tra ghế đã được chọn
+                {
+                    soLuongGhe++;
+                }
+            }
+
+        }
+        private void TinhThanhTien()
+        {
+
+            decimal tongTien = 0;
+
+            // Tính tiền cho từng loại vé đã chọn
+            tongTien += soLuongNguoiLon * giaVeNguoiLon;
+            tongTien += soLuongSV * giaVeSV;
+            tongTien += soLuongTreEm * giaVeTreEm;
+
+            // Cập nhật lại tổng tiền vào TextBox
+            txtThanhTien.Text = tongTien.ToString("N0");
+
+        }
+
+      
+        private void radNguoiLon_CheckedChanged_1(object sender, EventArgs e)
+        {
+ 
+            TinhGiaVe(); 
+            TinhThanhTien();
+        }
+
+        private void radSV_CheckedChanged_1(object sender, EventArgs e)
+        {
+        
+            TinhGiaVe(); 
+            TinhThanhTien();
+        }
+
+        private void radTreEm_CheckedChanged_1(object sender, EventArgs e)
+        {
+      
+            TinhGiaVe();
+            TinhThanhTien();
+        }
+
+        private void btnThanhToan_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Bạn có muốn thanh toán?", "Thông Báo", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                // Thông báo thanh toán thành công
+                MessageBox.Show("Thanh toán thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Đổi màu ghế đã chọn thành màu xám (hoặc bạn có thể chọn màu khác để đánh dấu ghế đã thanh toán)
+                foreach (Control control in panel2.Controls)
+                {
+                    if (control is Button btn && btn.BackColor == Color.Red)
+                    {
+                        btn.BackColor = Color.Gray;  // Đổi màu ghế đã chọn thành màu xám (hoặc màu khác nếu muốn)
+                    }
+                }
+
+                // Reset lại số lượng vé đã chọn
+                soLuongNguoiLon = 0;
+                soLuongSV = 0;
+                soLuongTreEm = 0;
+
+                // Reset lại tổng tiền
+                tongTien = 0;
+                txtThanhTien.Clear();  // Xóa hiển thị tổng tiền
+
+                // Reset lại các radio button (nếu cần thiết)
+                radNguoiLon.Checked = false;
+                radSV.Checked = false;
+                radTreEm.Checked = false;
+            }
+            
+        }
+    }
+    
+}
